bc5fb4b60799e04e0ba279d7ae92f9991b14a9d3
hernanjose <55003778+hernanjose@users.noreply.github.com>
2019-09-06 17:29:22 -0500

    Add files via upload

---
+++ kata2.java
@@ -0,0 +1,15 @@
+package bbva.apx.kata1;
+
+public class kata2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int dig=1;
+		if(dig % 2==0) {
+			System.out.println("par");
+		}else {
+			System.out.println("impar");
+		}
+	}
+
+}
